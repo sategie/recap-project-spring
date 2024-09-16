15bb4483df7115f92287c803b036e07c39bf1484
sategie <sategie@yahoo.com>
2024-09-17 01:15:57 +0200

    Add GET endpoint in Todo controller and add comments in Todo record.

--- src/main/java/org/example/recapprojectspring/Todo.java
+++ src/main/java/org/example/recapprojectspring/Todo.java
@@ -1,4 +1,5 @@
 package org.example.recapprojectspring;
 
+//Todo record which corresponds to the todo collection in MongoDB
 public record Todo(String id, String description, Status status) {
 }
--- src/main/java/org/example/recapprojectspring/TodoController.java
+++ src/main/java/org/example/recapprojectspring/TodoController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController// Grants RESTful abilities to the TodoController class
 @RequestMapping("api/todo")// Base URL for the controller's endpoints
 
@@ -15,7 +17,14 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+@GetMapping()// Defines a GET endpoint which handles GET requests to the base URL
 
+/*getToDos() method which calls the getAllTodos() method of the TodoService
+* The method retrieves a list of the todos
+* The list is then serialized by Spring and returned as JSON to the client*/
+    public List<Todo> getToDos(){
+        return todoService.getAllTodos();
+}
 
 
 
