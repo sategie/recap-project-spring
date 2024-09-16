b3298de91da8f931344cc438247a9fc6873ae498
sategie <sategie@yahoo.com>
2024-09-17 00:47:27 +0200

    Add comments in enum and TodoRepository.

--- src/main/java/org/example/recapprojectspring/Status.java
+++ src/main/java/org/example/recapprojectspring/Status.java
@@ -1,5 +1,6 @@
 package org.example.recapprojectspring;
 
+// Enum showing predefined statuses which never change
 public enum Status {
     COMPLETED,
     IN_PROGRESS,
--- src/main/java/org/example/recapprojectspring/TodoRepository.java
+++ src/main/java/org/example/recapprojectspring/TodoRepository.java
@@ -2,5 +2,10 @@ package org.example.recapprojectspring;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/*MongoRepository is a Spring data interface used to provide CRUD operations for MongoDB
+ * <Todo, String>: The first parameter 'Todo' specifies the type of entity managed by the repository .i.e Todo objects.
+ * The second parameter in the generics 'String' specifies the type of the ID field used in the Todo class.
+ * Here it will be an ID field of type String*/
+
 public interface TodoRepository extends MongoRepository<Todo, String> {
 }
