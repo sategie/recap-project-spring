3fdee52e8b62959d49d70552c5315fed66b637b1
sategie <sategie@yahoo.com>
2024-09-17 03:58:12 +0200

    Add comments to GlobalExceptionHandler class.

--- src/main/java/org/example/recapprojectspring/GlobalExceptionHandler.java
+++ src/main/java/org/example/recapprojectspring/GlobalExceptionHandler.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-@RestControllerAdvice// Annotation showing that the class provides global exception handling for all endpoints
+@RestControllerAdvice// Annotation showing that the class provides exception handling
 @ResponseStatus(HttpStatus.BAD_REQUEST) //Sets the default HTTP status to BAD_REQUEST
 
 public class GlobalExceptionHandler {
+    /*@ExceptionHandler defines a method which handles a type of Exception
+    * Exception.class means that the method will handle the Exception class*/
     @ExceptionHandler(Exception.class)
 
     public ErrorMessage handleGlobalExceptions(Exception e){
